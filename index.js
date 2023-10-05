9118aa13f50044d74698fa1fbcbf1221e0e47bc5
rebalelaawar <rebalelaawar@gmail.com>
2023-10-05 16:19:32 -0400

    Fetch lab

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json => {
+    renderBooks(json)
+   console.log(json[4].name)
   
-}
-
+})
+  )}
 function renderBooks(books) {
   const main = document.querySelector('main');
+
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
@@ -13,5 +18,6 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+  fetchBooks()
+  
 });
